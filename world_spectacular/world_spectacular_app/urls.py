7262f1a24c98ec30979ec62f43a2a2f8cf75dbc8
Andrea Clark <andrea_clark@glic.com>
2021-07-02 00:57:03 -0500

    adding secret key

--- world_spectacular/world_spectacular_app/urls.py
+++ world_spectacular/world_spectacular_app/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.landing_page, name='landing_page'),
+    # path('profiles/', views.get_users, name='get_users'),
+    # path('profiles/:pk/', views.get_user, name='get_user'),
+    # path('country/:abbr/', views.get_user, name='get_user'),
     # path('world/', views.get_countries, name='get_countries'),
 ]
