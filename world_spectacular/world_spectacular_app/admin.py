0cdcf9a4a738d71d20df4c33029aa3bfef84ab04
Andrea Clark <andrea_clark@glic.com>
2021-07-07 21:37:28 -0500

    superuser added

--- world_spectacular/world_spectacular_app/admin.py
+++ world_spectacular/world_spectacular_app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import User
+# from .models import Country
 
-admin.site.register(User)
+# admin.site.register(User_Visit)
+# admin.site.register(Country)
--- world_spectacular/world_spectacular_app/models.py
+++ world_spectacular/world_spectacular_app/models.py
@@ -7,12 +7,28 @@ class User(models.Model):
     name = models.CharField(max_length=50)
     photo_url = models.CharField(max_length=150, null=True)
     about_me = models.TextField(max_length=350, null=True)
-    country = models.CharField(max_length=50, null=True)
+    country_code = models.CharField(max_length=50, null=True)
 
     def __str__(self):
         return self.username
 
 # class User_Visit(models.Model): 
 #     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='visits')
+#     country_code = models.CharField(max_length=50, null=True)
 #     visited = models.BooleanField(default=False)
 #     dream_visit  = models.BooleanField(default=False)
+
+#     def __str__(self):        
+#         return (self.user + self.country_code)
+
+# class Country(models.Model): 
+#     country = models.CharField(max_length=100, null=False)
+#     abbr = models.CharField(max_length=3, null=False) 
+#     wiki_name = models.CharField(max_length=150, null=True)
+#     anthem = models.CharField(max_length=150, null=True)
+#     globe_map = models.CharField(max_length=150, null=True)
+#     picture1 = models.CharField(max_length=150, null=True)
+#     picture2 = models.CharField(max_length=150, null=True)
+
+#     def __str__(self):        
+#         return (self.country + self.abbr)
